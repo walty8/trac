c1915a73f213a26f6b54987cdf4c619a886d638e
Christian Boos <cboos@edgewall.org>
2016-03-17 00:42:46 +0100

    ticket: fix regression in conversion to RSS test, introduced in dc6fb98e

--- trac/ticket/tests/conversion.py
+++ trac/ticket/tests/conversion.py
@@ -146,7 +146,11 @@ class TicketConversionTestCase(unittest.TestCase):
             self.req, 'trac.ticket.Ticket', ticket, 'rss')
         self.maxDiff = None
         self.assertEqual(("""<?xml version="1.0"?>
+
+
+
 <rss version="2.0" xmlns:dc="http://purl.org/dc/elements/1.1/">
+
   <channel>
     <title>My Project: Ticket #1: Foo</title>
     <link>http://example.org/trac.cgi/ticket/1</link>
