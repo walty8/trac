3b9e31b0002a929d79b509995b6781e074b97df7
walty8 <walty8@gmail.com>
2016-04-15 06:26:11 +0000

    merged previous commit from patch file

--- trac/ticket/batch.py
+++ trac/ticket/batch.py
@@ -22,6 +22,7 @@ from trac.notification.api import NotificationSystem
 from trac.perm import IPermissionRequestor
 from trac.ticket import TicketSystem, Ticket
 from trac.ticket.notification import BatchTicketChangeEvent
+from trac.ticket.api import ITicketManipulator
 from trac.util.datefmt import datetime_now, parse_date, user_time, utc
 from trac.util.text import exception_to_unicode, to_unicode
 from trac.util.translation import _, tag_
@@ -41,6 +42,7 @@ class BatchModifyModule(Component):
 
     implements(IPermissionRequestor, IRequestHandler)
 
+    ticket_manipulators = ExtensionPoint(ITicketManipulator)
     is_valid_default_handler = False
 
     list_separator_re = re.compile(r'[;\s,]+')
@@ -66,8 +68,9 @@ class BatchModifyModule(Component):
 
         if new_values is not None:
             selected_tickets = self._get_selected_tickets(req)
-            self._save_ticket_changes(req, selected_tickets,
-                                      new_values, comment, action)
+            if self._validate_tickets(req, selected_tickets, new_values):
+                self._save_ticket_changes(req, selected_tickets,
+                                          new_values, comment, action)
 
         # Always redirect back to the query page we came from.
         req.redirect(req.session['query_href'])
@@ -160,25 +163,56 @@ class BatchModifyModule(Component):
             if action in actions:
                 yield controller
 
+    def _get_updated_ticket_values(self, req, ticket, new_values):
+        list_fields = self._get_list_fields()
+        _values = new_values.copy()
+        for field in list_fields:
+            if field in new_values:
+                old = ticket[field] if field in ticket else ''
+                new = new_values[field]
+                mode = req.args.get('batchmod_value_' + field +
+                                    '_mode')
+                new2 = req.args.get('batchmod_value_' + field +
+                                    '_secondary', '')
+                _values[field] = self._change_list(old, new, new2,
+                                                   mode)
+        return _values
+
+
+    def _validate_tickets(self, req, selected_tickets, new_values):
+        valid = True
+        for id in selected_tickets:
+            ticket = Ticket(self.env, int(id))
+            _values = self._get_updated_ticket_values(req, ticket, 
+                                                      new_values)
+
+            for field_name, field_value in _values.items():
+                ticket[field_name] = field_value
+
+            for manipulator in self.ticket_manipulators:
+                for field, message in manipulator.validate_ticket(req, 
+                                                                  ticket):
+                    valid = False
+                    if field:
+                        add_warning(req, tag_("The field %(field)s"
+                                              " for ticket #%(ticketId)s"
+                                              " is invalid: %(message)s",
+                                              field=tag.strong(field),
+                                              ticketId=id,
+                                              message=message))
+                    else:
+                        add_warning(req, message)
+        return valid
+
+
     def _save_ticket_changes(self, req, selected_tickets,
                              new_values, comment, action):
         """Save all of the changes to tickets."""
         when = datetime_now(utc)
-        list_fields = self._get_list_fields()
         with self.env.db_transaction as db:
             for id in selected_tickets:
                 t = Ticket(self.env, int(id))
-                _values = new_values.copy()
-                for field in list_fields:
-                    if field in new_values:
-                        old = t[field] if field in t else ''
-                        new = new_values[field]
-                        mode = req.args.get('batchmod_value_' + field +
-                                            '_mode')
-                        new2 = req.args.get('batchmod_value_' + field +
-                                            '_secondary', '')
-                        _values[field] = self._change_list(old, new, new2,
-                                                           mode)
+                _values = self._get_updated_ticket_values(req, t, new_values)
                 controllers = list(self._get_action_controllers(req, t,
                                                                 action))
                 for controller in controllers:
